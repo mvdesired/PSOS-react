6573a25c906e422d45202346b8051a53b78c17ab
Surbhi Sharma <surbhi91096@gmail.com>
2019-05-06 17:41:01 +0530

    Support.js

--- components/Support.js
+++ components/Support.js
@@ -20,8 +20,8 @@ class Support  extends Component{
                         <Image source={require('../assets/back-icon.png')} style={{width:10,height:19}}/>
                     </TouchableOpacity>
                     
-                    <Text style={{fontFamily:'AvenirLTStd-Roman',color:'#FFFFFF',fontSize:16,marginRight:130,flexWrap:'wrap'}}>Support</Text>
-                    <View style={{flexDirection:'row',marginLeft:10}}>
+                    <Text style={{fontFamily:'AvenirLTStd-Roman',color:'#FFFFFF',fontSize:16}}>Support</Text>
+                    <View style={{flexDirection:'row',alignItems:'center'}}>
                         <TouchableOpacity>
                         </TouchableOpacity>
                     </View>
@@ -29,7 +29,7 @@ class Support  extends Component{
 
                 <ScrollView style={{height:RemoveHiehgt,flex:1}}>
 
-                <View style={{paddingVertical:2,backgroundColor:'#FFFFFF',marginTop:5,paddingHorizontal:10}}>
+                <View style={{paddingVertical:2,backgroundColor:'#FFFFFF',marginTop:1,paddingHorizontal:10}}>
 
                   <View style={{justifyContent: 'center'}}>
                   <Text style={{fontFamily:'AvenirLTStd-Medium',fontSize:17,color:'#151515',marginTop:2,flexWrap:'wrap'}}>How can we help you ?</Text>
@@ -41,9 +41,9 @@ class Support  extends Component{
 
 
               <View style={{paddingHorizontal:10}}>
-                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:40,borderRadius:10,marginTop:20}}>
-                    <View style={{     }}>
-                    
+                <View style={{flex: 1,flexDirection:'row',marginVertical:10,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:30,borderRadius:4}}>
+                    <View style={{ flexDirection:'row',alignItems:'center' }}>
+
                     </View>
                     <Text style={{fontFamily:'AvenirLTStd-Medium',marginLeft:10,fontSize:20,color:'#147dbf',marginTop:2,flexWrap:'wrap'}}>Question ?</Text>
                 </View>
@@ -51,7 +51,7 @@ class Support  extends Component{
 
                
               <View style={{paddingHorizontal:10}}>
-                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:40,borderRadius:10}}>
+                <View style={{flex: 1,flexDirection:'row',marginVertical:1,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:30,borderRadius:4}}>
                     <View style={{       }}>
 
                     </View>
@@ -60,7 +60,7 @@ class Support  extends Component{
               </View>         
 
               <View style={{paddingHorizontal:10}}>
-                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:40,borderRadius:10}}>
+                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:30,borderRadius:4}}>
                     <View style={{       }}>
 
                     </View>
@@ -69,7 +69,7 @@ class Support  extends Component{
               </View>         
 
               <View style={{paddingHorizontal:10}}>
-                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:40, borderRadius:10}}>
+                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:30, borderRadius:4}}>
                     <View style={{       }}>
 
                     </View>
@@ -78,7 +78,7 @@ class Support  extends Component{
               </View>         
 
               <View style={{paddingHorizontal:10}}>
-                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:40,borderRadius:10}}>
+                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:30,borderRadius:4}}>
                     <View style={{       }}>
 
                     </View>
@@ -87,7 +87,7 @@ class Support  extends Component{
               </View>         
 
               <View style={{paddingHorizontal:10}}>
-                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:40,borderRadius:10}}>
+                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:30,borderRadius:4}}>
                     <View style={{       }}>
 
                     </View>
@@ -96,7 +96,7 @@ class Support  extends Component{
               </View>         
 
               <View style={{paddingHorizontal:10}}>
-                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:40,borderRadius:10}}>
+                <View style={{flex: 1,flexDirection:'row',marginVertical:5,paddingHorizontal:10,backgroundColor: '#FFFFFF',borderColor:'#1d7bc3',borderWidth:1,height:30,borderRadius:4}}>
                     <View style={{       }}>
 
                     </View>
